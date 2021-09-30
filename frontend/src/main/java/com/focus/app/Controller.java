132f1e060fd14c02834aeacd720c9e8deafb2f1b
Wojciech Kwaśniewicz <kwasniewicz.w@pg.com>
2021-09-30 13:14:34 +0200

    Test build

--- frontend/src/main/java/com/focus/app/Controller.java
+++ frontend/src/main/java/com/focus/app/Controller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/")
     String helloWorld(){
-        return "Hello World! ";
+        return "Hello World! Backend";
     }
 }
