3e83e5b40626d227153b92bc0ea3e61c8e61c63d
Wojciech Kwaśniewicz <kwasniewicz.w@pg.com>
2021-10-18 11:26:55 +0200

    Added selenium testing

--- backend/src/test/java/com/focus/backend/AppApplicationTests.java
+++ backend/src/test/java/com/focus/backend/AppApplicationTests.java
@@ -1,13 +1,38 @@
 package com.focus.backend;
 
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class AppApplicationTests {
+	private static WebDriver driver;
+
+	@BeforeAll
+	static void init(){
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
+	@AfterAll
+	static void shutDown(){
+		driver.quit();
+	}
 
 	@Test
-	void contextLoads() {
+	public void shouldNavigateToSignInGivenNoAuthentication(){
+		driver.get("http://localhost:4200");
+
+		boolean containsSignIn = StringUtils.containsIgnoreCase(driver.getPageSource(), "SIGN IN");
+
+		assertTrue(containsSignIn);
+		assertTrue(driver.getCurrentUrl().contains("login"));
 	}
 
 }
